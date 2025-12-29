f9b5767fbd9fcc1d683b4bd31ddc8fecfc83bdf9
Federico Gratti <federicogratti@gmail.com>
2025-12-29 09:21:44 -0300

    Added GoogleAnalytics component

---
+++ app/components/GoogleAnalytics.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import Script from 'next/script'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="beforeInteractive"
+      />
+      <Script id="google-analytics" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
+    </>
+  )
+}
