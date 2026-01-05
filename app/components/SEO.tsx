e2ac3a1a180ab2bd0fcfeb4fb545449c5273f6e5
Federico Gratti <federicogratti@gmail.com>
2026-01-04 21:04:42 -0300

    Added SEO paragraphs

--- app/components/SEO.tsx
+++ app/components/SEO.tsx
@@ -6,6 +6,26 @@ export default function SEO() {
     <>
       <p className="hidden">Technology has always been more than a job for me. I actively build applications in my free time and explore new tools out of curiosity. I’m a proactive and reliable engineer with strong communication skills, driven by continuous learning and meaningful problem-solving.</p>   
       <p className="hidden">I’m looking to join a company that values continuous professional and personal growth. I'm motivated by being part of collaborative teams that embrace new technologies and encourage constant learning.</p>
+      <p className="hidden">Designed and maintained production-ready Ruby on Rails APIs.</p>
+      <p className="hidden">Built an in-house JS framework focused on performant 3D web experiences.</p>
+      <p className="hidden">Implemented real-time communication between backend and frontend.</p>
+      <p className="hidden">Involved in the development of robust interactive web applications.</p>
+      <p className="hidden">Built automation tools that improved development workflows and efficiency.</p>
+      <p className="hidden">Led dailies with clients, aligning technical execution with business needs.</p>
+      <p className="hidden">Partnered with designers to deliver intuitive user experiences.</p>
+      <p className="hidden">Delivered 10+ e-commerce solutions using open source gems.</p>
+      <p className="hidden">Created custom applications tailored to specific Shopify store requirements.</p>
+      <p className="hidden">Built a backend system to interact and store data from a desktop application.</p>
+      <p className="hidden">Designed and Implemented tailor-made invoicing manager system.</p>
+      <p className="hidden">Contributed to a fully remote Scrum team, collaborating across time zones.</p>
+      <p className="hidden">Designed and Built CRUD-heavy Rails monolith applications from scratch.</p>
+      <p className="hidden">Developed backend systems to manage complex internal client workflows.</p>
+      <p className="hidden">Applied Scrum practices to ensure predictable and efficient delivery.</p>
+      <p className="hidden">Contributed to the design and implementation of robust software solutions.</p>
+      <p className="hidden">Built backend systems to support game features and data management.</p>
+      <p className="hidden">Implemented core gameplay mechanics, including tap-and-hold movement.</p>
+      <p className="hidden">Applied OOP principles such as inheritance, polymorphism, and abstraction.</p>
+      <p className="hidden">Skills: Ruby, JavaScript, TypeScript, Ruby on Rails, React.js, AWS, PostgreSQL, MongoDB, Redis, Heroku, Scrum.</p>
     </>
   )
 }
