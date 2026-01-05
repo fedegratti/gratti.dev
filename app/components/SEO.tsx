60db3042b66bb69e40443a415d1572c2caade526
Federico Gratti <federicogratti@gmail.com>
2026-01-04 21:09:53 -0300

    Update SEO.tsx

--- app/components/SEO.tsx
+++ app/components/SEO.tsx
@@ -6,6 +6,9 @@ export default function SEO() {
     <>
       <p className="hidden">Technology has always been more than a job for me. I actively build applications in my free time and explore new tools out of curiosity. I’m a proactive and reliable engineer with strong communication skills, driven by continuous learning and meaningful problem-solving.</p>   
       <p className="hidden">I’m looking to join a company that values continuous professional and personal growth. I'm motivated by being part of collaborative teams that embrace new technologies and encourage constant learning.</p>
+      <p className="hidden">Full Stack Developer</p>
+      <p className="hidden">Full Stack Engineer</p>
+      <p className="hidden">Ruby on Rails Developer</p>
       <p className="hidden">Designed and maintained production-ready Ruby on Rails APIs.</p>
       <p className="hidden">Built an in-house JS framework focused on performant 3D web experiences.</p>
       <p className="hidden">Implemented real-time communication between backend and frontend.</p>
